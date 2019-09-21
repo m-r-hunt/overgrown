c13de0a9407bdabb72d89e6cbcff3a09837cdb89
Max Hunt <max@huntw3.com>
2019-09-21 19:38:21 +0100

    Add catman movement

--- scripts/catman.gd
+++ scripts/catman.gd
@@ -1,5 +1,21 @@
-extends Sprite
+extends KinematicBody2D
+
+var player_speed := 100.0
 
 
 func _ready():
-	$"Animation Player".play("Walk")
+	$"Aseprite Sprite/Animation Player".play("Walk")
+
+
+func _physics_process(delta):
+	var dx = Vector2()
+	if Input.is_action_pressed("p1_down"):
+		dx.y = 1
+	if Input.is_action_pressed("p1_up"):
+		dx.y = -1
+	if Input.is_action_pressed("p1_left"):
+		dx.x = -1
+	if Input.is_action_pressed("p1_right"):
+		dx.x = 1
+	dx *= player_speed * delta
+	Utils.use(move_and_collide(dx))
