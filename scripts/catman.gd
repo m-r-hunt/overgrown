63309541b7095d30f810ac2aff5a1797e03d89e8
Max Hunt <max@huntw3.com>
2019-09-21 20:13:44 +0100

    Add player selector and fiddle with scaling

--- scripts/catman.gd
+++ scripts/catman.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 var player_speed := 100.0
+var last_select_dir := Vector2(0, 1)
 
 
 func _ready():
@@ -17,5 +18,10 @@ func _physics_process(delta):
 		dx.x = -1
 	if Input.is_action_pressed("p1_right"):
 		dx.x = 1
+	if dx != Vector2(0, 0):
+		last_select_dir = dx
 	dx *= player_speed * delta
 	Utils.use(move_and_collide(dx))
+	var selector_x = 8 + floor(global_position.x / 16)*16 + last_select_dir.x * 16
+	var selector_y = 8 + floor(global_position.y / 16)*16 + last_select_dir.y * 16
+	$"Selector".position = Vector2(selector_x, selector_y) - global_position
--- scripts/scale_control.gd
+++ scripts/scale_control.gd
@@ -6,6 +6,16 @@ export var pixel_scale := 1
 func _ready():
 	window_resize()
 	Utils.e_connect(get_viewport(), "size_changed", self, "window_resize")
+	
+
+func _process(_delta):
+	if Input.is_action_just_pressed("scale_up"):
+		pixel_scale += 1
+		window_resize()
+	if Input.is_action_just_pressed("scale_down") && pixel_scale >= 2:
+		pixel_scale -= 1
+		window_resize()
 
 func window_resize():
+	assert(pixel_scale > 0 && typeof(pixel_scale) == TYPE_INT)
 	get_viewport().set_size_override(true, OS.get_window_size() / pixel_scale)
