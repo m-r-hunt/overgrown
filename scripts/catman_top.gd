e6e25b4dd8b5b2686bdeb64b5c21960dd77fe842
Max Hunt <max@huntw3.com>
2019-09-22 22:37:00 +0100

    Test shader on catman

--- scripts/catman_top.gd
+++ scripts/catman_top.gd
@@ -4,8 +4,7 @@ export(int, 1, 4) var player_number := 1
 
 func _ready():
 	$KinematicBody2D.collision_layer |= (1 << player_number)
-	# TODO Set controls somehow
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	#var mat_copy = $"KinematicBody2D/Aseprite Sprite".material.duplicate()
+	#$"KinematicBody2D/Aseprite Sprite".material = mat_copy
+	randomize()
+	$"KinematicBody2D/Aseprite Sprite".material.set_shader_param("col", Color(randf(), randf(), randf()))
