a6ddfd9376a59a2b0ae3b1814d7dc2d8df24efb5
Max Hunt <max@huntw3.com>
2019-09-25 22:10:05 +0100

    Name property more sensibly

--- scripts/debug_helper.gd
+++ scripts/debug_helper.gd
@@ -2,5 +2,5 @@ extends Node
 
 
 func _ready():
-	if ProjectSettings.get_setting("my_settings/auto_full_screen"):
+	if ProjectSettings.get_setting("my_settings/auto_maximize"):
 		OS.window_maximized = true
