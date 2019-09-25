5491ec35a4142008ae6d01dc1b474f9e97f7bf0e
Max Hunt <max@huntw3.com>
2019-09-25 16:55:33 +0100

    Tidy carrot enum

--- scripts/carrot.gd
+++ scripts/carrot.gd
@@ -2,18 +2,18 @@ extends Node2D
 
 
 enum State {
-	Bag,
-	Growing,
-	Grown,
-	Wilted,
-	Item,
+	BAG,
+	GROWING,
+	GROWN,
+	WILTED,
+	ITEM,
 }
 
-export var state := State.Bag
+export var state := State.BAG
 
 func pickable():
 	match state:
-		State.Bag, State.Item, State.Grown:
+		State.BAG, State.ITEM, State.GROWN:
 			return true
 		_:
 			return false
@@ -24,7 +24,7 @@ func place(place):
 	get_parent().remove_child(self)
 	place.target.add_child(self)
 	position = Vector2.ZERO
-	state = State.Growing
+	state = State.GROWING
 	$AsepriteSprite/AnimationPlayer.play("Grow1")
 	var collider = preload("res://scenes/carrot_collider.tscn").instance()
 	add_child(collider)
