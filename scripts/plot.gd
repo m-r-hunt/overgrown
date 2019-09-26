fa339f25abe1c019e3bf1f0e65b5f3f51412f27b
Max Hunt <max@huntw3.com>
2019-09-26 08:18:59 +0100

    Start on watering

---
+++ scripts/plot.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func water():
+	$"AsepriteSprite/AnimationPlayer".play("Watered")
--- scripts/watering_can.gd
+++ scripts/watering_can.gd
@@ -3,3 +3,10 @@ extends Area2D
 
 func pickable():
 	return true
+
+
+func place(place: Place):
+	assert(place)
+	if place.target.has_method("water"):
+		place.target.water()
+
