96490713d2d56c4bc949b06e0576d432922e17b0
Max Hunt <max@huntw3.com>
2019-10-19 21:51:08 +0100

    Add title screen

---
+++ scripts/title.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+func _ready():
+	Utils.e_connect($PlayButton, "pressed", self, "on_play_button_pressed")
+	Utils.e_connect($QuitButton, "pressed", self, "on_quit_button_pressed")
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		play()
+	elif Input.is_action_just_pressed("ui_cancel"):
+		quit()
+
+
+func on_play_button_pressed():
+	play()
+
+
+func on_quit_button_pressed():
+	quit()
+
+
+func play():
+	var ret = get_tree().change_scene("res://scenes/farms.tscn")
+	assert(ret == OK)
+
+
+func quit():
+	get_tree().quit()
