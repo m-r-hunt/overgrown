49391ef0eb9e38525b2b31ca8bed934c32b1f947
Max Hunt <max@huntw3.com>
2019-10-28 22:35:02 +0000

    Improve use of debug settings

--- scripts/debug_helper.gd
+++ scripts/debug_helper.gd
@@ -1,6 +1,22 @@
 extends Node
 
 
+# Must be bools. Should turn on/off all permanent debug features this way.
+const debug_settings = [
+	"my_settings/auto_maximize",
+	"my_settings/short_rounds",
+]
+
+
 func _ready():
-	if ProjectSettings.get_setting("my_settings/auto_maximize"):
-		OS.window_maximized = true
+	if OS.is_debug_build():
+		print("I'm a debug build")
+		if ProjectSettings.get_setting("my_settings/auto_maximize"):
+			OS.window_maximized = true
+	else:
+		# Ensure debug settings are disabled for release builds
+		# Ideally we should expunge them all and also this script, 
+		# but this is useful until a really "final" state
+		print("I'm a release build")
+		for setting in debug_settings:
+			ProjectSettings.set_setting(setting, false)
