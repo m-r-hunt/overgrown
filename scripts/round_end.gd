d63a738f7b240d18f2efbf18524a2475bb5b0c63
Max Hunt <max@huntw3.com>
2019-10-29 12:44:48 +0000

    First attempt at bar height animation

--- scripts/round_end.gd
+++ scripts/round_end.gd
@@ -22,9 +22,23 @@ func _ready():
 			get_node("catman" + str(i+1)).queue_free()
 			get_node("Bar" + str(i+1)).queue_free()
 		else:
-			get_node("catman" + str(i+1)).position.y -= (PlayerStats.player_moneys[i] / max_money) * bar_height
-			get_node("Bar" + str(i+1)).margin_top -= (PlayerStats.player_moneys[i] / max_money) * bar_height
 			get_node("Money" + str(i+1)).text = "$" + str(PlayerStats.player_moneys[i])
+			$Tween.interpolate_property(
+				get_node("catman" + str(i+1)), 
+				"position", 
+				get_node("catman" + str(i+1)).position, get_node("catman" + str(i+1)).position + Vector2(0, -(PlayerStats.player_moneys[i] / max_money) * bar_height),
+				3.0,
+				Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
+			)
+			$Tween.interpolate_property(
+				get_node("Bar" + str(i+1)), 
+				"margin_top", 
+				get_node("Bar" + str(i+1)).margin_top, get_node("Bar" + str(i+1)).margin_top -(PlayerStats.player_moneys[i] / max_money) * bar_height,
+				3.0,
+				Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
+			)
+	$Tween.start()
+	yield($Tween, "tween_all_completed")
 	
 	if not tie:
 		$WinnerLabel.text = "Player " + str(winner+1) + " Wins!"
