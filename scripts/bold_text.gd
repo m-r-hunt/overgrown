f22798de2a91620b69670c8656946ae1a5aae2b6
Max Hunt <max@huntw3.com>
2019-11-03 16:19:45 +0000

    Fix bold text

--- scripts/bold_text.gd
+++ scripts/bold_text.gd
@@ -2,21 +2,29 @@ extends Label
 tool
 
 export var bold_text := "" setget set_text
-export(Font) var custom_font = null setget set_theme
+export(Font) var custom_font = preload("res://m5x7.tres") setget set_font
+var ready = false
+
+func _ready():
+	ready = true
+	set_text(bold_text)
+	set_font(custom_font)
 
 func set_text(val):
 	bold_text = val
 	text = val
-	$Outline1.text = val
-	$Outline2.text = val
-	$Outline3.text = val
-	$Outline4.text = val
+	if ready:
+		$Outline1.text = val
+		$Outline2.text = val
+		$Outline3.text = val
+		$Outline4.text = val
 
 
-func set_theme(t):
+func set_font(t):
 	custom_font = t
-	font = t
-	$Outline1.font = t
-	$Outline2.font = t
-	$Outline3.font = t
-	$Outline4.font = t
+	add_font_override("font", custom_font)
+	if ready:
+		$Outline1.add_font_override("font", t)
+		$Outline2.add_font_override("font", t)
+		$Outline3.add_font_override("font", t)
+		$Outline4.add_font_override("font", t)
