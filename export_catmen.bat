c1ac0aa0f7b2ac615b08f5a02b2c9b1867529d89
Max Hunt <max@huntw3.com>
2020-01-16 20:51:23 +0000

    Add plans and catman palette swap script

---
+++ export_catmen.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\Aseprite\Aseprite.exe" -b --sheet "sprites/catman1t.png" --data "sprites/catman1t.json" "sprites/catman4.ase" --palette "sprites/catman_palette1.aseprite"
+"C:\Program Files\Aseprite\Aseprite.exe" -b --sheet "sprites/catman2t.png" --data "sprites/catman2t.json" "sprites/catman4.ase" --palette "sprites/catman_palette2.aseprite"
+"C:\Program Files\Aseprite\Aseprite.exe" -b --sheet "sprites/catman3t.png" --data "sprites/catman3t.json" "sprites/catman4.ase" --palette "sprites/catman_palette3.aseprite"
+"C:\Program Files\Aseprite\Aseprite.exe" -b --sheet "sprites/catman4t.png" --data "sprites/catman4t.json" "sprites/catman4.ase" --palette "sprites/catman_palette4.aseprite"
