daa21b8c3c15e1d996e25f4e882213b6c1288390
Max Hunt <max@huntw3.com>
2020-01-23 16:48:23 +0000

    Redo planning notes/todos

---
+++ notes.gd
@@ -0,0 +1,36 @@
+extends Node
+
+## Near TODOs
+# Decide screen dimensions
+# Finish rebuilding farms in Tiled
+# Use autoload to store level info rather than injecting directly into farms scene instance
+# Move instructions to seperate screen
+
+## Far TODOs
+# Truck to sell seeds
+# Wandering pedestrians through middle
+# Wandering buyers in with pedestrians
+# Refill watering can
+
+## Planning
+#LAN Party Date/Deadline: 28th March 2020 (Saturday)
+
+#Now - 24th January (Shape Jam II)
+#1 week in Febuary (??? 8bti Jam)
+#29th February - 8th March (7DRL 2020)
+
+#Remaining: 7 Weeks
+
+#Minor improvements:
+#Improve control scheme (r stick "aiming", improved dash feel and feedback)
+#Improved sound and background music
+#More types of things to grow with balance improvements
+#Rocks and logs to clear out of field
+
+#Stretch Goal:
+#2v2 mode
+
+#Future improvements:
+#5 day ongoing structure
+#Farm upgrades
+#Animals
