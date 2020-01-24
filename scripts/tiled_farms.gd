473455b9b3966313138ad7a3b1d5f5f7225db06b
Max Hunt <max@huntw3.com>
2020-01-24 20:49:17 +0000

    More farm fixing

--- scripts/tiled_farms.gd
+++ scripts/tiled_farms.gd
@@ -1,6 +1,11 @@
 extends TileMap
 
 
+# TODO more global? Changing tile size is probably hard at this point.
+const TILE_SIZE = 16
+const HALF_TILE_SIZE = TILE_SIZE / 2
+
+
 func gate_only(farm):
 	get_node(farm + "/WateringCan").queue_free()
 	get_node(farm + "/Catman").queue_free()
@@ -10,11 +15,22 @@ func gate_only(farm):
 
 func _ready():
 	set_player_number("TopLeft", 1)
+	set_player_number("TopRight", 2)
+	set_player_number("BottomLeft", 3)
+	set_player_number("BottomRight", 4)
+	
+	place_plots(2, 9, 2, 5)
+	place_plots(22, 29, 2, 5)
+	place_plots(2, 9, 13, 16)
+	place_plots(22, 29, 13, 16)
+
+
+func place_plots(xmin, xmax, ymin, ymax):
 	var plot_prefab = preload("res://scenes/objects/plot.tscn")
-	var x = 48+8
-	while x <= 168+8:
-		var y = 48+8
-		while y <= 104+8:
+	var x = xmin*TILE_SIZE + HALF_TILE_SIZE
+	while x <= xmax*TILE_SIZE + HALF_TILE_SIZE:
+		var y = ymin*TILE_SIZE + HALF_TILE_SIZE
+		while y <= ymax*TILE_SIZE + HALF_TILE_SIZE:
 			var plot = plot_prefab.instance()
 			plot.position = Vector2(x, y)
 			plot.z_index = -1
