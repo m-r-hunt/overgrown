c6808ebeb4154b027cf2d16057710326fe7ad4e3
Max Hunt <max@huntw3.com>
2020-01-25 09:44:22 +0000

    Delete a bunch of unused stuff

--- scripts/conveyor_push.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-export var speed := 10.0
-export var direction := Vector2(0, 1)
-
-func _physics_process(delta):
-	for body in get_overlapping_bodies():
-		body.move_and_collide(direction * delta * speed)
