2e57511b8868b78025808f8f954ab8b46321c0cd
Max Hunt <max@huntw3.com>
2020-02-12 21:54:40 +0000

    Fiddle with timer a little bit

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -2,4 +2,7 @@ extends Node2D
 
 
 func _process(_delta: float):
-	$AsepriteSprite.frame = floor($AsepriteSprite.hframes * PlayerStats.get_time_portion())
+	var frame = ceil($AsepriteSprite.hframes * PlayerStats.get_time_portion())-1
+	if frame < 0:
+		frame = 0
+	$AsepriteSprite.frame = frame
