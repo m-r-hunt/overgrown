02f61b2738f77d266524c7a254c59b36f346ef4f
Max Hunt <max@huntw3.com>
2020-02-13 16:39:58 +0000

    Polish up timer some more and fix tomato sprite

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -1,8 +1,44 @@
 extends Node2D
 
 
+var main_sequence := false
+var playing := -1
+
+
+func _ready():
+	$AsepriteSprite/AnimationPlayer.get_animation("Idle").loop = false
+	$AsepriteSprite/AnimationPlayer.play("Idle")
+	Utils.e_connect(PlayerStats, "time_started", self, "on_time_started")
+	Utils.e_connect(PlayerStats, "time_up", self, "on_time_up")
+
+
 func _process(_delta: float):
-	var frame = ceil($AsepriteSprite.hframes * PlayerStats.get_time_portion())-1
-	if frame < 0:
-		frame = 0
-	$AsepriteSprite.frame = frame
+	if main_sequence:
+		var portion = PlayerStats.get_time_portion()
+		var stage := 1
+		if portion < 0.25:
+			stage = 1
+		elif portion >= 0.25 and portion < 0.5:
+			stage = 2
+		elif portion >= 0.5 and portion < 0.75:
+			stage = 3
+		else:
+			stage = 4
+		if stage != playing:
+			$AsepriteSprite/AnimationPlayer.play("Stage" + str(stage))
+			playing = stage
+
+
+func on_time_started():
+	$AsepriteSprite/AnimationPlayer.get_animation("Spin").loop = false
+	$AsepriteSprite/AnimationPlayer.play("Spin")
+	yield($AsepriteSprite/AnimationPlayer, "animation_finished")
+	$AsepriteSprite/AnimationPlayer.get_animation("Fall").loop = false
+	$AsepriteSprite/AnimationPlayer.play("Fall")
+	yield($AsepriteSprite/AnimationPlayer, "animation_finished")
+	main_sequence = true
+
+
+func on_time_up():
+	main_sequence = false
+	$AsepriteSprite/AnimationPlayer.play("Finish")
