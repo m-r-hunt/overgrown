b9a1572174c5979105ed7f8a888b353b6740593e
Max Hunt <max@huntw3.com>
2020-02-29 22:47:21 +0000

    Change button scene to a 2d node

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,4 +1,4 @@
-extends TextureButton
+extends Node2D
 
 tool
 
@@ -9,3 +9,11 @@ export var text := "" setget set_text
 func set_text(t: String):
 	text = t
 	$Label.text = t
+
+signal pressed
+
+func _ready():
+	Utils.e_connect($Button, "pressed", self, "on_pressed")
+
+func on_pressed():
+	emit_signal("pressed")
